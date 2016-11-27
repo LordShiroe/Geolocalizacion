292c974ca4db625f17d46ab07993ed91dd872b4b
Carlos Angarita <carlomagno127@gmail.com>
2016-11-27 18:14:08 -0500

    Primer Commit

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from authentication.models import Account
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Account)
+    barcode = models.TextField()
+    latitud = models.TextField()
+    longitud = models.TextField()
+    timestamp = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '{0}'.format(self.barcode)
